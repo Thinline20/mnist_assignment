3dd3ea9b97f44e27cc929d9353ccf9c08da8149f
pekochan069 <pekochan069@gmail.com>
2023-04-03 21:28:02 +0900

    Fixed issue

--- src/core/functions.py
+++ src/core/functions.py
@@ -22,7 +22,18 @@ def sigmoid(x):
     Returns:
         np.array: (1 / 1 + np.exp(-x))
     """
-    return np.array(1 / (1 + np.exp(-x)))
+    return 1 / (1 + np.exp(-x))
+
+def sigmoid_grad(x):
+    """sigmoid gradient
+
+    Args:
+        x (np.array): input data
+
+    Returns:
+        np.array: (1.0 - sigmoid(x)) * sigmoid(x)
+    """
+    return (1.0 - sigmoid(x)) * sigmoid(x)
 
 def relu(x):
     """ReLU function
@@ -33,7 +44,20 @@ def relu(x):
     Returns:
         np.array: x if bigger than 0, 0 otherwise
     """
-    return np.array(np.maximum(0, x))
+    return np.maximum(0, x) 
+
+def relu_grad(x):
+    """ReLU Gradient
+
+    Args:
+        x (np.array): input data
+        
+    Returns:
+        np.array
+    """
+    grad = np.zeros(x)
+    grad[x >= 0] = 1
+    return grad
 
 def softmax(x):
     """softmax
@@ -44,8 +68,14 @@ def softmax(x):
     Returns:
         np.array: (x - e^x) / (sum of x - e^x)
     """
-    t = np.exp(x - np.max(x))
-    return np.array(t / np.sum(t))
+    if x.ndim == 2:
+        x = x.T
+        x = x - np.max(x, axis=0)
+        y = np.exp(x) / np.sum(np.exp(x), axis=0)
+        return y.T
+    
+    x = x - np.max(x)
+    return np.exp(x) / np.sum(np.exp(x))
 
 # Loss Functions
 
@@ -61,8 +91,12 @@ def cee(y, t, delta=1e-7):
         float: cross entropy
     """
     if y.ndim == 1:
-        y = y.reshape(1, y.size)
         t = t.reshape(1, t.size)
-    
+        y = y.reshape(1, y.size)
+        
+    # 훈련 데이터가 원-핫 벡터라면 정답 레이블의 인덱스로 반환
+    if t.size == y.size:
+        t = t.argmax(axis=1)
+             
     batch_size = y.shape[0]
     return -np.sum(np.log(y[np.arange(batch_size), t] + delta)) / batch_size
--- src/network/twolayernet.py
+++ src/network/twolayernet.py
@@ -3,57 +3,74 @@ import numpy as np
 from core.functions import *
 from core.gradient import *
 
-class TwoLayerNet:
-    def __init__(
-        self, input_size: int, hidden_size: int, output_size: int, activation_fn, output_classifier, weight_init_std=0.01
-    ) -> None:
-        """Initialize Two Layer Network
 
-        Args:
-            input_size (int): size of input data
-            hidden_size (int): size of hidden layer
-            output_size (int): size of output
-            activation_fn (function): an activation function for the network
-            output_classifier (function): a function that classify the output
-            weight_init_std (float, optional): initial weight. Defaults to 0.01.
-        """
+class TwoLayerNet:
+    def __init__(self, input_size, hidden_size, output_size, weight_init_std=0.01):
+        # 가중치 초기화
         self.params = {}
         self.params["W1"] = weight_init_std * np.random.randn(input_size, hidden_size)
         self.params["B1"] = np.zeros(hidden_size)
         self.params["W2"] = weight_init_std * np.random.randn(hidden_size, output_size)
         self.params["B2"] = np.zeros(output_size)
 
-        self.activation_fn = activation_fn
-        self.output_classifier = output_classifier
-
-    def predict(self, X):
+    def predict(self, x):
         W1, W2 = self.params["W1"], self.params["W2"]
         B1, B2 = self.params["B1"], self.params["B2"]
 
-        Z = self.activation_fn(np.dot(X, W1) + B1)
-        Y = self.output_classifier(np.dot(Z, W2) + B2)
+        a1 = np.dot(x, W1) + B1
+        z1 = sigmoid(a1)
+        a2 = np.dot(z1, W2) + B2
+        y = softmax(a2)
 
-        return Y
+        return y
 
-    def loss(self, X, T):
-        return cee(self.predict(X), T)
+    # x : 입력 데이터, t : 정답 레이블
+    def loss(self, x, t):
+        y = self.predict(x)
 
-    def accuracy(self, X, T):
-        Y = self.predict(X)
-        Y = np.argmax(Y, axis=1)
-        T = np.argmax(T, axis=1)
+        return cee(y, t)
 
-        accuracy = np.sum(Y == T) / float(X.shape[0])
+    def accuracy(self, x, t):
+        y = self.predict(x)
+        y = np.argmax(y, axis=1)
+        t = np.argmax(t, axis=1)
 
+        accuracy = np.sum(y == t) / float(x.shape[0])
         return accuracy
 
-    def numerical_gradient(self, X, T):
-        loss = lambda _: self.loss(X, T)
-        
+    # x : 입력 데이터, t : 정답 레이블
+    def numerical_gradient(self, x, t):
+        loss_W = lambda W: self.loss(x, t)
+
         grads = {}
-        grads["W1"] = numerical_gradient(loss, self.params["W1"])
-        grads["B1"] = numerical_gradient(loss, self.params["B1"])
-        grads["W1"] = numerical_gradient(loss, self.params["W1"])
-        grads["B1"] = numerical_gradient(loss, self.params["B1"])
+        grads["W1"] = numerical_gradient(loss_W, self.params["W1"])
+        grads["B1"] = numerical_gradient(loss_W, self.params["B1"])
+        grads["W2"] = numerical_gradient(loss_W, self.params["W2"])
+        grads["B2"] = numerical_gradient(loss_W, self.params["B2"])
+
+        return grads
+
+    def gradient(self, x, t):
+        W1, W2 = self.params["W1"], self.params["W2"]
+        B1, B2 = self.params["B1"], self.params["B2"]
+        grads = {}
+
+        batch_num = x.shape[0]
+
+        # forward
+        a1 = np.dot(x, W1) + B1
+        z1 = sigmoid(a1)
+        a2 = np.dot(z1, W2) + B2
+        y = softmax(a2)
+
+        # backward
+        dy = (y - t) / batch_num
+        grads["W2"] = np.dot(z1.T, dy)
+        grads["B2"] = np.sum(dy, axis=0)
+
+        da1 = np.dot(dy, W2.T)
+        dz1 = sigmoid_grad(a1) * da1
+        grads["W1"] = np.dot(x.T, dz1)
+        grads["B1"] = np.sum(dz1, axis=0)
 
         return grads
