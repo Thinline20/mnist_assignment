73f8a46aa3526342a6cd5b3cd00f5450b1152aea
Thinline20 <thinline20@gmail.com>
2023-04-13 10:02:56 +0900

    Temporary save - AdamW Implementation

--- src/core/optimizer.py
+++ src/core/optimizer.py
@@ -1,3 +1,4 @@
+from enum import Enum
 import numpy as np
 
 class SGD:
@@ -48,7 +49,7 @@ class Adam:
         self.m = None
         self.v = None
         
-    def update(self, params, grads, delta=1e-7):
+    def update(self, params, grads, epsilon=1e-7):
         if self.m == None:
             self.m = {}
             self.v = {}
@@ -63,4 +64,31 @@ class Adam:
             self.m[k] += (1.0 - self.beta1) * (grads[k] - self.m[k])
             self.v[k] += (1.0 - self.beta2) * (grads[k] ** 2 - self.v[k])
             
-            params[k] -= (lr_t * self.m[k]) / (np.sqrt(self.v[k]) + delta)
+            params[k] -= (lr_t * self.m[k]) / (np.sqrt(self.v[k]) + epsilon)
+
+class AdamWScheduler(Enum):
+    Cosine_With_Restart = 1
+    Cosine = 2
+
+class AdamW:
+    def __init__(self, batch_size, train_data_count, epoch, lr=0.01, decay_rate_norm=0.5, beta1=0.9, beta2=0.999, scheduler=AdamWScheduler.Cosine_With_Restart) -> None:
+        self.lr = lr
+        self.decay_rate = decay_rate_norm * np.sqrt(batch_size / train_data_count * epoch)
+        self.beta1 = beta1
+        self.beta2 = beta2
+        self.iter = 0
+        self.m = None
+        self.v = None
+        self.scheduler = scheduler
+
+    def update(self, params, grads, epsilon=1e-7):
+        if self.m == None:
+            self.m = {}
+            self.v = {}
+            for k, v in params.items():
+                self.m[k] = np.zeros_like(v)
+                self.v[k] = np.zeros_like(v)
+
+        self.iter += 1
+        
+        
